653d9d15aafe837decae3e15453a62dddd51d6a2
overshiki <le.niu@hotmail.com>
2023-01-28 17:51:50 +0800

    change documentation

--- src/MonadInterface.jl
+++ src/MonadInterface.jl
@@ -2,7 +2,6 @@ module MonadInterface
 
 function unwrap end
 function fmap end 
-# function mbind end 
 
 """
 interface for Functions, note that in this case, instance of T should be callable, just as what an instance of Function does
@@ -13,14 +12,10 @@ const FunctionUnion = Union{FunctionType, Function}
 mbind(ma, f::FunctionUnion) = f(unwrap(ma))
 
 """
-since mbind is so convinient, we what an infix operator for it. In Haskell, we have >>=, however, in julia we can not define an operator with more than 2 special characters, thus symbols like >>= is not avaliable. Here, we instead use >>, note that in Haskell >> has a different meaning, but as long as we are in julia, we just omit this convention.
+since mbind is so convinient, we what an infix operator for it. In Haskell, we have >>=, however, in julia we can not define an operator with symbols like >>=. Here, we instead use >>>.
 """
 (>>>)(ma, f::FunctionUnion) = mbind(ma, f)
 
-# """
-# one good news is that we can also define the reverse order operator as (>>)
-# """
-# (<<<)(f::FunctionUnion, ma) = mbind(ma, f)
 
 # function join end 
 # function mreturn end
@@ -40,6 +35,5 @@ function check_rules(f, g, m, ::Val{:fgm})
     @assert mbind(mbind(m, f), g) == mbind(m, x -> mbind(f(x), g))
 end
 
-# export unwrap, fmap, mbind, mreturn, mthen, check_rules
 
 end # module MonadInterface
